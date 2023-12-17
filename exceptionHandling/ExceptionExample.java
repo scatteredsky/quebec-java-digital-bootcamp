64f71f28311fd475eabf04de33eb794a76a900e0
Serena Terra <67299222+scatteredsky@users.noreply.github.com>
2023-12-16 19:09:32 -0500

    Exception Handling: example

--- exceptionHandling/ExceptionExample.java
+++ exceptionHandling/ExceptionExample.java
@@ -1,2 +1,18 @@
-package exceptionHandling;public class ExceptionExample {
+package exceptionHandling;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ExceptionExample extends Exception {
+    public static void main(String[] args) {
+        Number price;
+
+        try {
+            price = NumberFormat.getInstance().parse("a1.75");
+            System.out.println(price);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            System.out.println("The value informed is not a number.");
+        }
+    }
 }
